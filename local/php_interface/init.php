6eb4edfba5972112f91c0fb4c357fa89da908232
my_git <@89872336622@yandex.ru>
2023-10-03 19:29:14 +0300

    сделал 4 задание

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,8 +1,88 @@
 <?php
 
-use App\PageViewed;
+/**
+ * @var CMain $APPLICATION
+ */
+
 use Bitrix\Main\EventManager;
 
 require_once __DIR__ . '/vendor/autoload.php';
 require_once($_SERVER["DOCUMENT_ROOT"] . "/local/components/custom/form/models/UsersFromForm.php");
 require_once __DIR__ . '/my_tools.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+EventManager::getInstance()->addEventHandler("iblock", "OnBeforeIBlockElementUpdate", function (&$arFields) {
+    /**
+     * @var CMain $APPLICATION
+     */
+    global $APPLICATION;
+
+    if ($arFields['ACTIVE'] === 'Y') return;
+    if ($arFields["IBLOCK_ID"] !== (int)$_ENV['NEWS_IBLOCK_ID']) return;
+
+    $data = CIBlockElement::GetByID($arFields['ID'])->Fetch();
+    if (!$data) return;
+
+    $currentDate = new \Bitrix\Main\Type\DateTime();
+    $creationDate = \Bitrix\Main\Type\DateTime::createFromTimestamp(
+        MakeTimeStamp($data["DATE_ACTIVE_FROM"])
+    );
+    $daysDifference = $currentDate->getTimestamp() - $creationDate->getTimestamp();
+    $daysDifference = floor($daysDifference / (60 * 60 * 24));
+
+    if ($daysDifference > 3) return;
+    $APPLICATION->ThrowException("Вы деактивировали свежую новость");
+    return false;
+});
+
+
+EventManager::getInstance()->addEventHandler("iblock", "OnBeforeIBlockElementDelete", function ($ID) {
+    /**
+     * @var CMain $APPLICATION
+     */
+    global $APPLICATION, $DB;
+    $data = CIBlockElement::GetByID($ID)->Fetch();
+
+    if (!$data) return;
+
+    if ((int)$data["IBLOCK_ID"] !== (int)$_ENV['PRODUCT_IBLOCK_ID']) return;
+
+    $count = (int)$data['SHOW_COUNTER'];
+    if ($count <= 0) return;
+
+    $DB->Query("LOCK TABLES b_iblock_element_property WRITE");
+    $DB->Query("UNLOCK TABLES");
+
+    (new CIBlockElement())->Update($ID, ["ACTIVE" => 'N']);
+    $APPLICATION->ThrowException("Нельзя удалить запись! Запись деактивирована!");
+    return false;
+});
+
+
+EventManager::getInstance()->addEventHandler("main", "OnAfterUserUpdate", function (&$arFields) {
+    /**
+     * @var CUser $USER
+     */
+    $contentGroupID = (int)$_ENV['CONTENT_GROUP_ID'];
+    $groupsUpdatedIds = array_map(fn ($val) => (int)$val["GROUP_ID"], $arFields["GROUP_ID"]);
+    if (!in_array($contentGroupID, $groupsUpdatedIds)) return;
+
+    $userGroupIds = CUser::GetUserGroup($arFields['ID']);
+    if (!is_array($userGroupIds)) return;
+    if (!in_array(strval($contentGroupID), $userGroupIds)) return;
+
+    $user = CUser::GetByID($arFields['ID'])->Fetch();
+    if (!$user) return;
+
+
+    $email = $user["EMAIL"];
+    $login = $user["LOGIN"];
+    CEvent::Send('NEW_CONTENT_MANAGER', "s1", [
+        "EMAIL" => $email,
+        "LOGIN" => $login,
+    ]);
+
+    error_log("Send mail on  $email for user $login");
+});
--- otzyvy/index.php
+++ otzyvy/index.php
@@ -6,11 +6,11 @@ $APPLICATION->SetTitle("Отзывы");
  <br>
 </div>
 <div>
-    <h2>Типо карусель</h2>
+	<h2>Типо карусель</h2>
 	 <?$APPLICATION->IncludeComponent(
 	"bitrix:news.list",
 	"news_list_default",
-	array(
+	Array(
 		"ACTIVE_DATE_FORMAT" => "d.m.Y",
 		"ADD_SECTIONS_CHAIN" => "N",
 		"AJAX_MODE" => "N",
@@ -22,6 +22,69 @@ $APPLICATION->SetTitle("Отзывы");
 		"CACHE_GROUPS" => "Y",
 		"CACHE_TIME" => "36000000",
 		"CACHE_TYPE" => "A",
+		"CHECK_DATES" => "Y",
+		"COMPONENT_TEMPLATE" => "news_list_default",
+		"DETAIL_URL" => "",
+		"DISPLAY_BOTTOM_PAGER" => "N",
+		"DISPLAY_DATE" => "Y",
+		"DISPLAY_NAME" => "Y",
+		"DISPLAY_PICTURE" => "Y",
+		"DISPLAY_PREVIEW_TEXT" => "Y",
+		"DISPLAY_TOP_PAGER" => "N",
+		"FIELD_CODE" => array(0=>"ID",1=>"CODE",2=>"XML_ID",3=>"NAME",4=>"DETAIL_TEXT",5=>"",),
+		"FILTER_NAME" => "",
+		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+		"IBLOCK_ID" => "4",
+		"IBLOCK_TYPE" => "rest_entity",
+		"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+		"INCLUDE_SUBSECTIONS" => "Y",
+		"MESSAGE_404" => "",
+		"NEWS_COUNT" => "4",
+		"PAGER_BASE_LINK_ENABLE" => "N",
+		"PAGER_DESC_NUMBERING" => "N",
+		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+		"PAGER_SHOW_ALL" => "N",
+		"PAGER_SHOW_ALWAYS" => "N",
+		"PAGER_TEMPLATE" => ".default",
+		"PAGER_TITLE" => "Новости",
+		"PARENT_SECTION" => "",
+		"PARENT_SECTION_CODE" => "",
+		"PREVIEW_TRUNCATE_LEN" => "",
+		"PROPERTY_CODE" => array(0=>"DOLZHNOST",1=>"COMPANY_NAME",2=>"",),
+		"SET_BROWSER_TITLE" => "N",
+		"SET_LAST_MODIFIED" => "N",
+		"SET_META_DESCRIPTION" => "N",
+		"SET_META_KEYWORDS" => "N",
+		"SET_STATUS_404" => "N",
+		"SET_TITLE" => "N",
+		"SHOW_404" => "N",
+		"SORT_BY1" => "ACTIVE_FROM",
+		"SORT_BY2" => "SORT",
+		"SORT_ORDER1" => "DESC",
+		"SORT_ORDER2" => "ASC",
+		"STRICT_SECTION_CHECK" => "N"
+	)
+);?>
+</div>
+<div>
+ <br>
+</div>
+<h2>Случайный отзыв</h2>
+ <?$APPLICATION->IncludeComponent(
+	"bitrix:news.list",
+	"random_news",
+	Array(
+		"ACTIVE_DATE_FORMAT" => "d.m.Y",
+		"ADD_SECTIONS_CHAIN" => "N",
+		"AJAX_MODE" => "N",
+		"AJAX_OPTION_ADDITIONAL" => "",
+		"AJAX_OPTION_HISTORY" => "N",
+		"AJAX_OPTION_JUMP" => "N",
+		"AJAX_OPTION_STYLE" => "Y",
+		"CACHE_FILTER" => "N",
+		"CACHE_GROUPS" => "Y",
+		"CACHE_TIME" => "36000000",
+		"CACHE_TYPE" => "N",
 		"CHECK_DATES" => "Y",
 		"DETAIL_URL" => "",
 		"DISPLAY_BOTTOM_PAGER" => "N",
@@ -30,14 +93,7 @@ $APPLICATION->SetTitle("Отзывы");
 		"DISPLAY_PICTURE" => "Y",
 		"DISPLAY_PREVIEW_TEXT" => "Y",
 		"DISPLAY_TOP_PAGER" => "N",
-		"FIELD_CODE" => array(
-			0 => "ID",
-			1 => "CODE",
-			2 => "XML_ID",
-			3 => "NAME",
-			4 => "DETAIL_TEXT",
-			5 => "",
-		),
+		"FIELD_CODE" => array("ID",""),
 		"FILTER_NAME" => "",
 		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
 		"IBLOCK_ID" => "4",
@@ -45,7 +101,7 @@ $APPLICATION->SetTitle("Отзывы");
 		"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
 		"INCLUDE_SUBSECTIONS" => "Y",
 		"MESSAGE_404" => "",
-		"NEWS_COUNT" => "4",
+		"NEWS_COUNT" => "1",
 		"PAGER_BASE_LINK_ENABLE" => "N",
 		"PAGER_DESC_NUMBERING" => "N",
 		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
@@ -56,11 +112,7 @@ $APPLICATION->SetTitle("Отзывы");
 		"PARENT_SECTION" => "",
 		"PARENT_SECTION_CODE" => "",
 		"PREVIEW_TRUNCATE_LEN" => "",
-		"PROPERTY_CODE" => array(
-			0 => "DOLZHNOST",
-			1 => "COMPANY_NAME",
-			2 => "",
-		),
+		"PROPERTY_CODE" => array("",""),
 		"SET_BROWSER_TITLE" => "N",
 		"SET_LAST_MODIFIED" => "N",
 		"SET_META_DESCRIPTION" => "N",
@@ -68,86 +120,18 @@ $APPLICATION->SetTitle("Отзывы");
 		"SET_STATUS_404" => "N",
 		"SET_TITLE" => "N",
 		"SHOW_404" => "N",
-		"SORT_BY1" => "ACTIVE_FROM",
+		"SORT_BY1" => "rand",
 		"SORT_BY2" => "SORT",
-		"SORT_ORDER1" => "DESC",
+		"SORT_ORDER1" => "ASC",
 		"SORT_ORDER2" => "ASC",
-		"STRICT_SECTION_CHECK" => "N",
-		"COMPONENT_TEMPLATE" => "news_list_default"
-	),
-	false
-);?>
-</div>
-<div>
- <br>
-</div>
-<h2>Случайный отзыв</h2> <?$APPLICATION->IncludeComponent("bitrix:news.list", "random_news", Array(
-	"ACTIVE_DATE_FORMAT" => "d.m.Y",	// Формат показа даты
-		"ADD_SECTIONS_CHAIN" => "N",	// Включать раздел в цепочку навигации
-		"AJAX_MODE" => "N",	// Включить режим AJAX
-		"AJAX_OPTION_ADDITIONAL" => "",	// Дополнительный идентификатор
-		"AJAX_OPTION_HISTORY" => "N",	// Включить эмуляцию навигации браузера
-		"AJAX_OPTION_JUMP" => "N",	// Включить прокрутку к началу компонента
-		"AJAX_OPTION_STYLE" => "Y",	// Включить подгрузку стилей
-		"CACHE_FILTER" => "N",	// Кешировать при установленном фильтре
-		"CACHE_GROUPS" => "Y",	// Учитывать права доступа
-		"CACHE_TIME" => "36000000",	// Время кеширования (сек.)
-		"CACHE_TYPE" => "A",	// Тип кеширования
-		"CHECK_DATES" => "Y",	// Показывать только активные на данный момент элементы
-		"DETAIL_URL" => "",	// URL страницы детального просмотра (по умолчанию - из настроек инфоблока)
-		"DISPLAY_BOTTOM_PAGER" => "N",	// Выводить под списком
-		"DISPLAY_DATE" => "Y",	// Выводить дату элемента
-		"DISPLAY_NAME" => "Y",	// Выводить название элемента
-		"DISPLAY_PICTURE" => "Y",	// Выводить изображение для анонса
-		"DISPLAY_PREVIEW_TEXT" => "Y",	// Выводить текст анонса
-		"DISPLAY_TOP_PAGER" => "N",	// Выводить над списком
-		"FIELD_CODE" => array(	// Поля
-			0 => "ID",
-			1 => "",
-		),
-		"FILTER_NAME" => "",	// Фильтр
-		"HIDE_LINK_WHEN_NO_DETAIL" => "N",	// Скрывать ссылку, если нет детального описания
-		"IBLOCK_ID" => "4",	// Код информационного блока
-		"IBLOCK_TYPE" => "rest_entity",	// Тип информационного блока (используется только для проверки)
-		"INCLUDE_IBLOCK_INTO_CHAIN" => "N",	// Включать инфоблок в цепочку навигации
-		"INCLUDE_SUBSECTIONS" => "Y",	// Показывать элементы подразделов раздела
-		"MESSAGE_404" => "",	// Сообщение для показа (по умолчанию из компонента)
-		"NEWS_COUNT" => "1",	// Количество новостей на странице
-		"PAGER_BASE_LINK_ENABLE" => "N",	// Включить обработку ссылок
-		"PAGER_DESC_NUMBERING" => "N",	// Использовать обратную навигацию
-		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",	// Время кеширования страниц для обратной навигации
-		"PAGER_SHOW_ALL" => "N",	// Показывать ссылку "Все"
-		"PAGER_SHOW_ALWAYS" => "N",	// Выводить всегда
-		"PAGER_TEMPLATE" => ".default",	// Шаблон постраничной навигации
-		"PAGER_TITLE" => "Новости",	// Название категорий
-		"PARENT_SECTION" => "",	// ID раздела
-		"PARENT_SECTION_CODE" => "",	// Код раздела
-		"PREVIEW_TRUNCATE_LEN" => "",	// Максимальная длина анонса для вывода (только для типа текст)
-		"PROPERTY_CODE" => array(	// Свойства
-			0 => "",
-			1 => "",
-		),
-		"SET_BROWSER_TITLE" => "N",	// Устанавливать заголовок окна браузера
-		"SET_LAST_MODIFIED" => "N",	// Устанавливать в заголовках ответа время модификации страницы
-		"SET_META_DESCRIPTION" => "N",	// Устанавливать описание страницы
-		"SET_META_KEYWORDS" => "N",	// Устанавливать ключевые слова страницы
-		"SET_STATUS_404" => "N",	// Устанавливать статус 404
-		"SET_TITLE" => "N",	// Устанавливать заголовок страницы
-		"SHOW_404" => "N",	// Показ специальной страницы
-		"SORT_BY1" => "rand",	// Поле для первой сортировки новостей
-		"SORT_BY2" => "SORT",	// Поле для второй сортировки новостей
-		"SORT_ORDER1" => "ASC",	// Направление для первой сортировки новостей
-		"SORT_ORDER2" => "ASC",	// Направление для второй сортировки новостей
-		"STRICT_SECTION_CHECK" => "N",	// Строгая проверка раздела для показа списка
-		"COMPONENT_TEMPLATE" => ".default"
-	),
-	false
+		"STRICT_SECTION_CHECK" => "N"
+	)
 );?><br>
- <h2>Просто список отзывов</h2>
+<h2>Просто список отзывов</h2>
  <?$APPLICATION->IncludeComponent(
 	"bitrix:news.list",
 	"news_list_default",
-	array(
+	Array(
 		"ACTIVE_DATE_FORMAT" => "d.m.Y",
 		"ADD_SECTIONS_CHAIN" => "N",
 		"AJAX_MODE" => "N",
@@ -160,6 +144,7 @@ $APPLICATION->SetTitle("Отзывы");
 		"CACHE_TIME" => "36000000",
 		"CACHE_TYPE" => "A",
 		"CHECK_DATES" => "Y",
+		"COMPONENT_TEMPLATE" => "news_list_default",
 		"DETAIL_URL" => "",
 		"DISPLAY_BOTTOM_PAGER" => "Y",
 		"DISPLAY_DATE" => "Y",
@@ -167,14 +152,7 @@ $APPLICATION->SetTitle("Отзывы");
 		"DISPLAY_PICTURE" => "Y",
 		"DISPLAY_PREVIEW_TEXT" => "Y",
 		"DISPLAY_TOP_PAGER" => "N",
-		"FIELD_CODE" => array(
-			0 => "ID",
-			1 => "CODE",
-			2 => "XML_ID",
-			3 => "NAME",
-			4 => "DETAIL_TEXT",
-			5 => "",
-		),
+		"FIELD_CODE" => array(0=>"ID",1=>"CODE",2=>"XML_ID",3=>"NAME",4=>"DETAIL_TEXT",5=>"",),
 		"FILTER_NAME" => "",
 		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
 		"IBLOCK_ID" => "4",
@@ -193,11 +171,7 @@ $APPLICATION->SetTitle("Отзывы");
 		"PARENT_SECTION" => "",
 		"PARENT_SECTION_CODE" => "",
 		"PREVIEW_TRUNCATE_LEN" => "",
-		"PROPERTY_CODE" => array(
-			0 => "DOLZHNOST",
-			1 => "COMPANY_NAME",
-			2 => "",
-		),
+		"PROPERTY_CODE" => array(0=>"DOLZHNOST",1=>"COMPANY_NAME",2=>"",),
 		"SET_BROWSER_TITLE" => "N",
 		"SET_LAST_MODIFIED" => "N",
 		"SET_META_DESCRIPTION" => "N",
@@ -209,9 +183,6 @@ $APPLICATION->SetTitle("Отзывы");
 		"SORT_BY2" => "SORT",
 		"SORT_ORDER1" => "DESC",
 		"SORT_ORDER2" => "ASC",
-		"STRICT_SECTION_CHECK" => "N",
-		"COMPONENT_TEMPLATE" => "news_list_default"
-	),
-	false
-);?>
- <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+		"STRICT_SECTION_CHECK" => "N"
+	)
+);?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
